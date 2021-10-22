c8f6dca471c81e8741b46fbbb150bfb30e45de85
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-10-22 12:35:36 -0300

    implementing a category update

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -14,7 +14,7 @@ router.post("/categories/save",(req,res) =>{ //crud insert
                         title: title,
                         slug: slugify(title)
                 }).then(()=>{
-                        res.redirect("/");
+                        res.redirect("/admin/categories/");
                 })
                  
         }else{
@@ -53,4 +53,39 @@ router.post("/categories/delete",(req,res) =>{ //crud delete
         }
 
 })
+
+router.get("/admin/categories/edit/:id",(req,res)=>{// pegando o Id pelo get através do edit e passando para varíavel category e renderizando a view de edit 
+       var id = req.params.id; 
+       if(isNaN(id)){
+        res.redirect("/admin/categories")
+       } 
+       
+       Category.findByPk(id).then(category =>{
+        if(id != undefined){
+               res.render("admin/categories/edit",{category:category});
+       
+       }
+        else{
+                res.redirect("/admin/categories")
+        }}).catch(erro =>{
+                res.redirect("/admin/categories")
+        })
+
+        
+});
+
+router.post("/categories/update", (req, res)=>{//crud update
+        var id = req.body.id
+        var title = req.body.title
+        Category.update({title:title},{
+                where:{
+                        id:id
+                }
+        }).then(()=>{
+                res.redirect("/admin/categories")
+        })
+
+        
+})
+
 module.exports= router
