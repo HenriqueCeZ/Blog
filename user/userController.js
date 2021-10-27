2068cb97069cd4b749cd1b86d0389e696da6170b
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-10-26 22:51:49 -0300

    login authentication with session release

--- user/userController.js
+++ user/userController.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 const User = require("./user")
 const Category = require("../categories/Category")
-const bcrypt = require("bcryptjs")
+var bcrypt = require('bcrypt')
+const adminAuth = require('../middlewares/adminAuth')
 
-router.get("/admin/users",(req,res) =>{
+router.get("/admin/users",adminAuth,(req,res) =>{
     res.send("Listagem de usuários")
 })
 
@@ -12,6 +13,9 @@ router.get("/admin/users/create",(req,res)=>{
     Category.findAll().then(category => {
         res.render("admin/users/create",{categories: category})
     })
+    if(req.session.users){
+        res.redirect('/admin/articles/articles');
+    }
 })
 
 router.get("/admin/users/login",(req,res)=>{
@@ -21,18 +25,19 @@ router.get("/admin/users/login",(req,res)=>{
 })
 
 router.post("/admin/user/create",(req,res)=>{
-    var login = req.body.email
-    var senha = req.body.senha
+    var email = req.body.email
+    var password = req.body.password
+  
 
     User.findOne({
-        where:{email:login}
+        where:{email:email}
     }).then(user =>{
         if(user == undefined){
             var salt = bcrypt.genSaltSync(10)
-            var hash = bcrypt.hashSync(senha,salt)
-            if(login!== undefined){
+            var hash = bcrypt.hashSync(password,salt)
+            if(email!== undefined){
                 User.create({
-                 login:login,
+                 email:email,
                  password:hash
                         }).then(()=>{
                          res.redirect("/")
@@ -50,23 +55,39 @@ router.post("/admin/user/create",(req,res)=>{
 })
 
 router.post("/admin/user/login",(req,res)=>{
-    var login = req.body.email
-    var senha = req.body.senha
-    if((login && senha) !== undefined){
-    User.findOne({
-       where:{ login:login,
-        password:senha}
-    }).then( user=>{
-       if(user != undefined){
-           res.render("admin/categories",{user:user})
-       }
-       else{
-           res.send("Senha incorreta")
-       }
+    var email = req.body.email
+    var password = req.body.password
+    
+    User.findOne({where:{email:email}}).then(users =>{
+        if(!users){
+            res.redirect("admin/users/create")
+            
+            }else{
+            bcrypt.compare(password, users.password	,( err, result)=>{
+                if (result == true){
+                   
+                    req.session.users ={
+                        id: users.id,
+                        email: users.email
+                    }
+                    res.redirect('/admin/articles/articles');
+                }else{
+                    res.redirect("/admin/users/create")
+                    
+                   
+                }
+            })
+         
+        }
     })
-    }else{
-        res.redirect("admin/users/login")
-    }
+    
+    
+
 })
+router.get("",authAdmin,(req,res)=>{
+    
+})
+
+
 
 module.exports= router
